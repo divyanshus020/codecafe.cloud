55dbd9730d8e85dc712a581ce7b2360da6d650a3
divyanshus020 <sharmadivyanshu281@gmail.com>
2024-03-28 22:31:21 +0530

    landing page done

---
+++ src/components/Btn.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Btn() {
+    return (
+        <div className='mt-2'>
+            <div>
+                <button class="btn-53">
+                    <div class="original">Login</div>
+                    <div class="letters">
+
+                        <span>S</span>
+                        <span>I</span>
+                        <span>g</span>
+                        <span>h</span>
+                        <span>-</span>
+                        <span>u</span>
+                        <span>p</span>
+                    </div>
+                </button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Btn
--- src/components/HomeLanding.jsx
+++ src/components/HomeLanding.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
-
+import Btn from './Btn'
+import LandingVideo from "../assets/landing.mp4"
 function HomeLanding() {
     return (
-        <section>
-            <div className='HomeLanding-1'>
-                
-            </div>
-        </section>
+        <>
+            <section>
+                <div className="video-container">
+                    <video autoPlay muted loop id="bg-video">
+                        <source src={LandingVideo} type="video/mp4" />
+                        Your browser does not support HTML5 video.
+                    </video>
+                    <div className="content-on-top">
+
+                    </div>
+                </div>
+
+                <main>
+                    <div class="hero absolute top-[300px] right-40">
+                        <div class="hero-text">
+                            <div className='bg-[#760fcb]'>
+                                <h1>
+                                    CodeCafe:<br /> Ignite Your Passion.</h1>
+                            </div>
+
+                            <div className='bg-white'>
+                                <p>Elevate your programming skills, solve challenges, and unlock the world of coding possibilities.</p>
+                            </div>
+                            
+                            <Btn/>
+                        </div>
+                        <div class="hero-image">
+                            {/* <!-- Add your hero image here --> */}
+                        </div>
+                    </div>
+                </main>
+            </section>
+
+
+        </>
     )
 }
 
